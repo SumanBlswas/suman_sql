bfc420a3571b5f60c640d09dfa86e5367276f7c8
Suman Biswas <sumanmanner@gmail.com>
2023-07-25 23:11:12 +0530

    upadted

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   try {
-    res.status(200).send("Welcome! to The Home");
+    res.status(200).send({
+      EndPoints: [
+        { "For GET": "http://16.171.148.3:3001/blogs" },
+        { "For GET with ID": "http://16.171.148.3:3001/blogs/:id" },
+        { "For POST": "http://16.171.148.3:3001/blogs" },
+        { "For PUT": "http://16.171.148.3:3001/blogs/:id" },
+        { "For DELETE with ID": "http://16.171.148.3:3001/blogs/:id" },
+      ],
+    });
   } catch (error) {
     res.status(400).send(error.message);
   }
